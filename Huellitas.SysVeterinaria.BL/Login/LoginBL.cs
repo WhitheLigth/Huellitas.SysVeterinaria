596722666bfc7e4f15d4d7ce3df9c7ce6c7a232b
Adrian hernandez <adrianhernandez3p0@outlook.es>
2024-02-19 23:19:49 -0600

    Programacion de la clase Login en capa BL

---
+++ Huellitas.SysVeterinaria.BL/Login/LoginBL.cs
@@ -0,0 +1,27 @@
+﻿using Huellitas.SysVeterinaria.DAL.Login;
+using Huellitas.SysVeterinaria.EN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huellitas.SysVeterinaria.BL.Login
+{
+    public class LoginBL
+    {
+        #region Metodo para crear un usuario nuevo
+        public static async Task<int> CreateUserAsync(UserEN user)
+        {
+            return await LoginDAL.CreateUserAsync(user);
+        }
+        #endregion
+
+        #region Metodo para iniciar sesion
+        public static async Task<UserEN> GetUserByIdAsync(UserEN user)
+        {
+            return await LoginDAL.GetUserByIdAsync(user);
+        }
+        #endregion
+    }
+}

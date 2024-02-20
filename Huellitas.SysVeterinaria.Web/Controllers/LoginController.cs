1f3aca23578b7c727c8e95d94604be5a31938edb
Osmany Lopez <osmany203lopez@gmail.com>
2024-02-20 17:45:47 -0600

    Se agrego y se programo el controlador de Login

---
+++ Huellitas.SysVeterinaria.Web/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+
+using Huellitas.SysVeterinaria.BL.Login;
+using Huellitas.SysVeterinaria.EN;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace Huellitas.SysVeterinaria.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        #region Encriptar clave
+        public static string ConvertirSha256(string texto)
+        {
+            StringBuilder Sb = new StringBuilder();
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(texto));
+
+                foreach (byte b in result)
+                    Sb.Append(b.ToString("x2"));
+            }
+            return Sb.ToString();
+        }
+        #endregion
+
+        #region login
+        public async Task<IActionResult> Login(int id)
+        {
+            var user = await LoginBL.GetUserByIdAsync(new UserEN { Id = id }); //Convertimos para mostrar muy bien 
+            return View("Index", "Home");
+        }
+        #endregion
+    }
+}

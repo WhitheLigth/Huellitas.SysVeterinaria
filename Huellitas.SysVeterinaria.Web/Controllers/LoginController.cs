2c5a0d907c96b60b92fda08abbaddf087ffe9357
Adrian hernandez <adrianhernandez3p0@outlook.es>
2024-02-21 13:32:13 -0600

    Update de LoginController

--- Huellitas.SysVeterinaria.Web/Controllers/LoginController.cs
+++ Huellitas.SysVeterinaria.Web/Controllers/LoginController.cs
@@ -11,23 +11,6 @@ namespace Huellitas.SysVeterinaria.Web.Controllers
 {
     public class LoginController : Controller
     {
-
-        #region Encriptar clave
-        public static string ConvertirSha256(string texto)
-        {
-            StringBuilder Sb = new StringBuilder();
-            using (SHA256 hash = SHA256Managed.Create())
-            {
-                Encoding enc = Encoding.UTF8;
-                byte[] result = hash.ComputeHash(enc.GetBytes(texto));
-
-                foreach (byte b in result)
-                    Sb.Append(b.ToString("x2"));
-            }
-            return Sb.ToString();
-        }
-        #endregion
-
         #region login
         public async Task<IActionResult> Login(int id)
         {

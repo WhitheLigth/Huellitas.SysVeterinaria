f6fb0eaea2f99a9ace417738ca7566d7f9293d07
Kathleen Calderon <cl23005@esfe.agape.edu.sv>
2024-02-22 14:36:36 -0600

    Creacion de la tabla personal

--- Huellitas.SysVeterinaria.DAL/Document/ScriptPrincipal.sql
+++ Huellitas.SysVeterinaria.DAL/Document/ScriptPrincipal.sql
@@ -2,3 +2,27 @@ CREATE DATABASE SysHuellitaVeterinariaDB;
 GO
 USE SysHuellitaVeterinariaDB;
 GO
+CREATE TABLE Employees(
+Id INT NOT NULL PRIMARY KEY IDENTITY (1,1),
+[Name] VARCHAR (50) NOT NULL,
+LastName VARCHAR (50) NOT NULL,
+Dui VARCHAR (10) NOT NULL,
+BirthDate DATE  NOT NULL,
+Age VARCHAR (3) NOT NULL.
+Gender VARCHAR (10) NOT NULL,
+CivilStatus VARCHAR (30) NOT NULL,
+[Address] VARCHAR (100) NOT NULL,
+Phone VARCHAR (10) NOT NULL,
+Email VARCHAR (50) NOT NULL,
+EmergencyNumber VARCHAR (10) NOT NULL,
+AcademicTitle VARCHAR (100) NOT NULL,
+Certification VARCHAR (100) NOT NULL,
+WorkExperience VARCHAR (100) NOT NULL,
+AreaOfSpecialization VARCHAR (100) NOT NULL,
+Position VARCHAR (100) NOT NULL,
+KnownAllergies VARCHAR(100) NOT NULL,
+RelevantMedicalConditions VARCHAR (100) NOT NULL,
+CreationDate DATETIME NOT NULL,
+ModificationDate DATETIME DEFAULT NULL,
+);
+GO

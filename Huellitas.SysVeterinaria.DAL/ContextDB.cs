5a613374bdc3a503b49df0ceae141a4b733a48fd
Hector Garcia <hg425823@gmail.com>
2024-03-15 22:38:56 -0600

    Insercion de Buenas Practicas y Formato a la clase ContextDB

--- Huellitas.SysVeterinaria.DAL/ContextDB.cs
+++ Huellitas.SysVeterinaria.DAL/ContextDB.cs
@@ -1,11 +1,14 @@
 ﻿#region REFERENCIAS
-using Huellitas.SysVeterinaria.EN.Employee_EN;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+// Referencias necesarias para el correcto funcionamiento
+using Huellitas.SysVeterinaria.EN.Employee_EN;
+using Microsoft.EntityFrameworkCore;
+
+
 
 #endregion
 
@@ -13,14 +16,16 @@ namespace Huellitas.SysVeterinaria.DAL
 {
     public class ContextDB : DbContext
     {
-
+        #region REFERENCIAS DE LAS TABLAS DE LA BD
         public DbSet<Employee> Employees { get; set; } //Coleccion que hace referencia a la tabla de la base de datos
 
 
+        #endregion
+
+        // Metodo de Conexion a la Base de Datos
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-ND37IL0\SQLEXPRESS;Initial Catalog=SysHuellitaVeterinariaDB;
-                                        Integrated Security=True; Encrypt = False; Trust Server Certificate=True"); //Poner str de concexion local
+            optionsBuilder.UseSqlServer(@""); //Poner str de concexion local
         }
     }
 }

56cb7c9f2709bdd6e70e71806899d2cd153f1c47
Adrian hernandez <adrianhernandez3p0@outlook.es>
2024-03-19 13:54:04 -0600

    Tarea Completada / Creacion y Programacion de la clase raza en la carpeta raza EN

---
+++ Huellitas.SysVeterinaria.EN/Breed EN/Breed.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huellitas.SysVeterinaria.EN.Breed_EN
+{
+    public class Breed
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [Display (Name = "Nombre")]
+        public string Name { get; set; }
+    }
+}

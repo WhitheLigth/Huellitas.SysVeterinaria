1311ed65652d0894dae61ca4938addb1c167c581
Adrian hernandez <adrianhernandez3p0@outlook.es>
2024-03-20 16:17:09 -0600

    Tarea Completada / 4- Programar la clase de RolesBL

---
+++ Huellitas.SysVeterinaria.BL/Role - BL/RoleBL.cs
@@ -0,0 +1,44 @@
+﻿using Huellitas.SysVeterinaria.DAL.Rol___DAL;
+using Huellitas.SysVeterinaria.EN.Rol_EN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huellitas.SysVeterinaria.BL.Role___BL
+{
+    public class RoleBL
+    {
+        public async Task<int> CreateAsync(Role role)
+        {
+            return await RoleDAL.CreateAsync(role);
+        }
+
+        public async Task<int> UpdateAsync(Role role)
+        {
+            return await RoleDAL.UpdateAsync(role);
+        }
+
+        public async Task<int> DeleteAsync(Role role)
+        {
+            return await RoleDAL.DeleteAsync(role);
+        }
+
+        public async Task<Role> GetByIdAsync(Role role)
+        {
+            return await RoleDAL.GetByIdAsync(role);
+        }
+
+        public async Task<List<Role>> GetAllAsync()
+        {
+            return await RoleDAL.GetAllAsync();
+        }
+
+        public async Task<List<Role>> SearchAsync(Role role)
+        {
+            return await RoleDAL.SearchAsync(role);
+        }
+
+    }
+}

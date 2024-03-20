2cd4c2c3d24a87c07f0558a8396bdde51acafd9a
Osmany Lopez <osmany203lopez@gmail.com>
2024-03-19 18:07:49 -0600

    Se agrego referencia de Breeds hacia la base de datos en la ContextDB

--- Huellitas.SysVeterinaria.DAL/ContextDB.cs
+++ Huellitas.SysVeterinaria.DAL/ContextDB.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Huellitas.SysVeterinaria.EN.Breed_EN;
+
 // Referencias necesarias para el correcto funcionamiento
 using Huellitas.SysVeterinaria.EN.Employee_EN;
 using Huellitas.SysVeterinaria.EN.Pet_Owner_EN;
@@ -24,6 +26,8 @@ namespace Huellitas.SysVeterinaria.DAL
         public DbSet<Service> Services { get; set; } // Coleccion que hacer referencia a la tabla de la base de datos
 
         public DbSet<PetOwner> PetOwners { get; set; } //Coleccion que hace referencia a la tabla de la base de datos
+
+        public DbSet<Breed> Breeds { get; set; } //Coleccion que hace referencia a la tabla de la base de datos
         #endregion
 
         // Metodo de Conexion a la Base de Datos

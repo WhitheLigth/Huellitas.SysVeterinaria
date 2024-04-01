7575406d83d9e690adb9a6d8c061c7c23944bcf5
Hector Garcia <hg425823@gmail.com>
2024-04-01 13:09:24 -0600

    Insercion de esquelo para la clase PositionBL

---
+++ Huellitas.SysVeterinaria.BL/Position - BL/PositionBL.cs
@@ -0,0 +1,47 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias necesarias para el correcto funcionamiento
+
+
+
+#endregion
+
+namespace Huellitas.SysVeterinaria.BL.Position___BL
+{
+    public class PositionBL
+    {
+        #region METODO PARA GUARDAR
+        // Metodo para guardar un nuevo registro a la base de datos
+
+        #endregion
+
+        #region METODO PARA MODIFICAR
+        // Metodo para modificar un registro existente en la base de datos
+
+        #endregion
+
+        #region METODO PARA ELIMINAR
+        // Metodo para eliminar un registro extente en la base de datos
+
+        #endregion
+
+        #region METODO PARA OBTENER POR ID
+        // Metodo para obtener un registro en base al id
+
+        #endregion
+
+        #region METODO PARA MOSTRAR LA LISTA DE REGISTROS
+        // Metodo para obtener toda la lista de registros y mostrarlos
+
+        #endregion
+
+        #region METODO PARA BUSCAR
+        // Metodo para buscar registros en la base de datos
+
+        #endregion
+    }
+}

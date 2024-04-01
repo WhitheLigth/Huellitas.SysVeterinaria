0e5c194378b01b0129b57ef7e09a23b2e4a0fc8a
Hector Garcia <hg425823@gmail.com>
2024-04-01 14:01:53 -0600

    Creacion de metodos CRUD para la Clase BL de PositionBL

--- Huellitas.SysVeterinaria.BL/Position - BL/PositionBL.cs
+++ Huellitas.SysVeterinaria.BL/Position - BL/PositionBL.cs
@@ -5,7 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 // Referencias necesarias para el correcto funcionamiento
-
+using Huellitas.SysVeterinaria.EN.Position_EN;
+using Huellitas.SysVeterinaria.DAL.Position___DAL;
 
 
 #endregion
@@ -16,32 +17,50 @@ namespace Huellitas.SysVeterinaria.BL.Position___BL
     {
         #region METODO PARA GUARDAR
         // Metodo para guardar un nuevo registro a la base de datos
-
+        public async Task<int> CreateAsync(Position position)
+        {
+            return await PositionDAL.CreateAsync(position);
+        }
         #endregion
 
         #region METODO PARA MODIFICAR
         // Metodo para modificar un registro existente en la base de datos
-
+        public async Task<int> UpdateAsync(Position position)
+        {
+            return await PositionDAL.UpdateAsync(position);
+        }
         #endregion
 
         #region METODO PARA ELIMINAR
         // Metodo para eliminar un registro extente en la base de datos
-
+        public async Task<int> DeleteAsync(Position position)
+        {
+            return await PositionDAL.DeleteAsync(position);
+        }
         #endregion
 
         #region METODO PARA OBTENER POR ID
         // Metodo para obtener un registro en base al id
-
+        public async Task<Position> GetByIdAsync(Position position)
+        {
+            return await PositionDAL.GetByIdAsync(position);
+        }
         #endregion
 
         #region METODO PARA MOSTRAR LA LISTA DE REGISTROS
         // Metodo para obtener toda la lista de registros y mostrarlos
-
+        public async Task<List<Position>> GetAllAsync()
+        {
+            return await PositionDAL.GetAllAsync();
+        }
         #endregion
 
         #region METODO PARA BUSCAR
         // Metodo para buscar registros en la base de datos
-
+        public async Task<List<Position>> SearchAsync(Position position)
+        {
+            return await PositionDAL.SearchAsync(position);
+        }
         #endregion
     }
 }
